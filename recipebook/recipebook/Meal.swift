249cb2da2d5fafcd7568a9d887cb771cd0d35130
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-04-05 17:09:46 +1000

    part 1 exercise 1

---
+++ recipebook/recipebook/Meal.swift
@@ -0,0 +1,18 @@
+//
+//  Meal.swift
+//  recipebook
+//
+//  Created by Andre Pham on 5/4/21.
+//
+
+import UIKit
+
+class Meal: NSObject {
+
+    // MARK: - Properties
+    
+    var name: String
+    var instructions: String
+    var ingredients: [IngredientMeasurement]
+    
+}
