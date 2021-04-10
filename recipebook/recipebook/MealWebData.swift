81780ec4231d3adfe85241508824e5c6536e6da2
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-04-11 03:29:57 +1000

    filtered ingredient names and measures

--- recipebook/recipebook/MealWebData.swift
+++ recipebook/recipebook/MealWebData.swift
@@ -15,8 +15,11 @@ class MealWebData: NSObject, Decodable {
     var mealName: String?
     var mealInstructions: String?
     
-    //var mealIngredientNames: [String?] // To preserve order of ingredients
-    //var mealIngedients: [String?: String?]
+    var mealIngredientNames = [String]()
+    var mealIngredientMeasurements = [String]()
+    
+    let INGREDIENT_NAME_KEY = "strIngredient"
+    let INGREDIENT_MEASURE_KEY = "strMeasure"
     
     // MARK: - Coding Keys
     
@@ -66,17 +69,42 @@ class MealWebData: NSObject, Decodable {
         
         // https://swiftsenpai.com/swift/decode-dynamic-keys-json/
         let infoContainer = try decoder.container(keyedBy: DynamicCodingKeys.self)
+        
+        var tempIngredientNames = [String: String]() // e.g. strIngredient1: Eggs
+        var tempIngredientMeasurements = [String: String]() // e.g. strMeasure1: 100g
+        
         for key in infoContainer.allKeys {
             do {
                 let decodedObject = try infoContainer.decode(String.self, forKey: DynamicCodingKeys(stringValue: key.stringValue)!)
-                print(key.stringValue)
-                print(decodedObject)
+                //print(key.stringValue)
+                //print(decodedObject)
+                if key.stringValue.contains(self.INGREDIENT_NAME_KEY) && decodedObject.count > 0 {
+                    //print(key.stringValue)
+                    //print(decodedObject)
+                    tempIngredientNames[key.stringValue] = decodedObject
+                }
+                else if key.stringValue.contains(self.INGREDIENT_MEASURE_KEY) && decodedObject.count > 0 {
+                    tempIngredientMeasurements[key.stringValue] = decodedObject
+                }
             }
             catch {
-                print("nil")
+                // null value
             }
             
         }
+        
+        for ingredientNum in 1...tempIngredientNames.count {
+            let ingredientNameKey = self.INGREDIENT_NAME_KEY + String(ingredientNum)
+            let ingredientMeasurementKey = self.INGREDIENT_MEASURE_KEY + String(ingredientNum)
+            
+            self.mealIngredientNames.append(tempIngredientNames[ingredientNameKey]!)
+            self.mealIngredientMeasurements.append(tempIngredientMeasurements[ingredientMeasurementKey]!)
+        }
+        
+        for i in 0...self.mealIngredientNames.count-1 {
+            print(self.mealIngredientNames[i] + ": " + self.mealIngredientMeasurements[i])
+        }
+        print("-----")
         
         
         /*
