4e422ea4555d0fdd7def5ab174ee48f724e90fe0
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-04-11 20:38:12 +1000

    can now search meals in api

--- recipebook/recipebook/SearchMealsTableViewController.swift
+++ recipebook/recipebook/SearchMealsTableViewController.swift
@@ -19,7 +19,10 @@ class SearchMealsTableViewController: UITableViewController {
     let SECTION_SHOWN_MEALS: Int = 0
     let SECTION_NEW_MEAL: Int = 1
     
+    var indicator = UIActivityIndicatorView()
+    
     // Other properties
+    // REMOVE RETRIEVED MEALS
     var shownMeals: [MealData] = []
     var retrievedMeals: [MealData] = []
     
@@ -31,26 +34,60 @@ class SearchMealsTableViewController: UITableViewController {
         
         // Creates search object
         let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search Meals"
         navigationItem.searchController = searchController
         
         // Ensure search is always visible
         navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // Add a loading indicator view
+        indicator.style = UIActivityIndicatorView.Style.large
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(indicator)
+        
+        NSLayoutConstraint.activate([
+            indicator.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            indicator.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
     }
     
-    func requestMealsWebData() {
+    func requestMealsWebData(searchText: String) {
+        
+        // https://www.themealdb.com/api/json/v1/1/search.php?s=cake
+        var requestURLComponents = URLComponents()
+        requestURLComponents.scheme = "https"
+        requestURLComponents.host = "www.themealdb.com"
+        requestURLComponents.path = "/api/json/v1/1/search.php"
+        requestURLComponents.queryItems = [
+            URLQueryItem(
+                name: "s",
+                value: searchText
+            )
+        ]
+        
+        guard let requestURL = requestURLComponents.url else {
+            print("Invalid URL.")
+            return
+        }
+        
+        /*
         guard let requestURL = URL(string: "https://www.themealdb.com/api/json/v1/1/search.php?s=cake") else {
             print("Invalid URL.")
             return
         }
+        */
         
         // Parse data
         let task = URLSession.shared.dataTask(with: requestURL) {
             (data, response, error) in
             
             // Occurs on a new thread
+            
+            DispatchQueue.main.async {
+                self.indicator.stopAnimating()
+            }
             
             // If we have recieved an error message
             if let error = error {
@@ -69,10 +106,15 @@ class SearchMealsTableViewController: UITableViewController {
                     for mealWebData in meals {
                         let mealName = mealWebData.mealName ?? ""
                         let mealInstructions = mealWebData.mealInstructions ?? ""
-                        self.retrievedMeals.append(MealData(name: mealName, instructions: mealInstructions, ingredients: mealWebData.mealIngredients))
+                        self.shownMeals.append(MealData(name: mealName, instructions: mealInstructions, ingredients: mealWebData.mealIngredients))
+                        //print(mealName)
                         //let name = ingredientWebData.ingredientName ?? ""
                         //let description = ingredientWebData.ingredientDescription ?? ""
                         //let _ = self.databaseController?.addIngredient(name: name, ingredientDescription: description)
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
                     }
                 }
             }
@@ -159,37 +201,49 @@ class SearchMealsTableViewController: UITableViewController {
         }
     }
     
-    /// DELETE OR USE LATER
-    func retrieveMeals() {
-        // Pass
-    }
-    
 }
 
 // MARK: - Protocol Extensions
 
-extension SearchMealsTableViewController: UISearchResultsUpdating {
+extension SearchMealsTableViewController: UISearchBarDelegate {
     
+    /*
     /// Called every time a change is detected in the search bar, and filters the shown meals to match the search, refreshing the TableView cells
     func updateSearchResults(for searchController: UISearchController) {
         guard let searchText = searchController.searchBar.text?.lowercased() else {
             return
         }
         
-        self.requestMealsWebData()
+        self.shownMeals = []
+        self.requestMealsWebData(searchText: searchText)
         
         if searchText.count > 0 {
-            self.shownMeals = self.retrievedMeals.filter({
-                // Return all meals that contain the search text
-                (meal: MealData) -> Bool in return (meal.name?.lowercased().contains(searchText) ?? false)
-            })
+            //self.shownMeals.removeAll()
         }
         else {
             // No meals are shown if there is no search input
-            shownMeals.removeAll()
+            self.shownMeals.removeAll()
         }
         
         tableView.reloadData()
+    }
+     */
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        shownMeals.removeAll()
+        tableView.reloadData()
+        
+        guard let searchText = searchBar.text?.lowercased() else {
+            return
+        }
+        
+        // Stops all existing tasks to avoid background download
+        // Causes: [NSURLSession sharedSession] may not be invalidated
+        URLSession.shared.invalidateAndCancel()
+        
+        // If there is search text, feedback is provided
+        indicator.startAnimating()
+        self.requestMealsWebData(searchText: searchText)
     }
     
 }
